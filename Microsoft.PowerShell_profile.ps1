d8a8edbc55cf4992c12dc667235b29e6e4c9b1c7
Maxime Labelle <m.labelle@criteo.com>
2020-01-23 13:27:28 +0100

    Added support for a separate profile script holding sensitive info.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -14,6 +14,21 @@ if (-not (Test-Path env:\PATH_ORIG)) {
 $Env:PATH = "$Env:PATH_ORIG;$_joinedpaths"
 $Env:GNUPGHOME = "$Env:APPDATA\GnuPG"
 
+## SECURITY - SENSITIVE DATA
+
+$secrets = $profile.Replace("profile", "secret-profile")
+Write-Host $secrets
+if (Test-Path -Path $secrets){
+    Write-Host "Loading sensitive data from $($secrets)"
+    . $secrets
+}
+
+## SENSITIVE DATA
+
+Function c {
+    param([string] $path = ".")
+    . code $path
+}
 Function b64 {
     [cmdletBinding()]
     param(
@@ -27,9 +42,25 @@ Function b64 {
 }
 Function add { git add $args }
 Function clone { git clone --recurse-submodules $args }
-Function commit { git commit -m $args }
+Function commit { git commit $args }
+Function cguid { [Guid]::NewGuid().guid | clipp }
 Function cwd { $PWD.Path | clipp }
-Function ewd { explorer $PWD.Path }
+Function ewd { param([string] $path = $PWD.Path) explorer $path }
+Set-Alias -Name e -Value ewd
+Function feature {
+    param(
+        [string]$feature
+    )
+
+    if ($feature.Length -gt 0) {
+        feature-start -feature $feature
+    }
+    else {
+        $branch = $(git rev-parse --abbrev-ref HEAD)
+        $feature = $branch.Replace("feature/", "")
+        Write-Output $feature
+    }
+}
 Function feature-start {
     param(
         [string]$feature
@@ -60,6 +91,17 @@ Function lol { git log --oneline --decorate --graph }
 Function pro { Set-Location C:\Projects }
 Function pull { git fetch -p; git merge --ff-only }
 Function push { git push $args }
+Function reset {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [Alias("FullName")]
+        [string]$path
+    )
+    PROCESS{
+        git checkout HEAD -- $path
+    }
+}
 Function run-tests { Get-ChildItem -Path $PATH -Recurse -Filter *Tests.csproj |% { dotnet test $_.FullName } }
 Function Search-Item {
     [CmdletBinding()]
@@ -75,9 +117,9 @@ Function Search-Item {
 
 Set-Alias -Name search -Value Search-Item
 
-Function sshlinux { ssh -i $Env:USERPROFILE\.ssh\maxime.key maxime`@40.85.93.164 $args }
+Function sshlinux { param([string]$username = "maxime") PROCESS { ssh -i $Env:USERPROFILE\.ssh\$userName.key $userName`@40.85.93.164 $args } }
 Function sshdown { param([string]$source, [string]$target) scp -i $Env:USERPROFILE\.ssh\maxime.key "maxime@40.85.93.164:$source" "$target" }
-Function sshup { param([string]$source, [string]$target) scp -i $Env:USERPROFILE\.ssh\maxime.key $source "maxime@40.85.93.164:$target" }
+Function sshup { param([string]$source, [string]$target, [string] $username = "maxime") scp -i $Env:USERPROFILE\.ssh\$username.key $source "$username@40.85.93.164:$target" }
 Function ub64 {
     [cmdletBinding()]
     param(
