3fe772db9719592cbd4ea704f732c22a69ad11dd
Maxime LABELLE <maxime.labelle@chanel.com>
2020-09-08 08:19:10 +0200

    Added vs shortcut command to launch Visual Studio.

--- Microsoft.PowerShell_dotnet-profile.ps1
+++ Microsoft.PowerShell_dotnet-profile.ps1
@@ -1,2 +1,9 @@
-Function pro { Set-Location C:\Projects }
-Function run-tests { Get-ChildItem -Path $PWD -Recurse -Filter *Tests.csproj | % { dotnet test $_.FullName } }
+ "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE", `
+ "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin", `
+ "C:\Portable Apps\IlSpy\IlSpy.exe" | Add-DirectoryToPath
+
+Function me { Set-Location D:\Projects\springcomp }
+Function pro { Set-Location D:\Projects }
+Function run-tests { Get-ChildItem -Path $PATH -Recurse -Filter *Tests.csproj | % { dotnet test $_.FullName } }
+
+Function vs { & devenv.exe $args }
