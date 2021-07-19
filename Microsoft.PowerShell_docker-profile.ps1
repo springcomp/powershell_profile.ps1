2a89347ba651143e4148d9d85ac8c7ec641351a5
Maxime LABELLE <maxime.labelle@chanel.com>
2021-07-19 08:36:23 +0200

    Fixed hardcoded path.

--- Microsoft.PowerShell_docker-profile.ps1
+++ Microsoft.PowerShell_docker-profile.ps1
@@ -11,7 +11,7 @@ if ($completions.IsPresent) {
 
     ## CLI completions require Git bash
 
-    "d:\users\mlabelle\appdata\local\programs\git\bin" | Add-DirectoryToPath -Prepend
+    "$Env:LOCALAPPDATA\programs\git\bin" | Add-DirectoryToPath -Prepend
 
     ## Install-Module DockerCompletion -Scope CurrentUser
     Import-Module DockerCompletion
