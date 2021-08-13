b8cc25f350adecdbc289188ee56c3e5777e00af5
Maxime Labelle <springcomp@users.noreply.github.com>
2021-08-13 21:44:25 +0200

    Improved compatibility with Windows PowerShell.

--- Microsoft.PowerShell_psreadline-profile.ps1
+++ Microsoft.PowerShell_psreadline-profile.ps1
@@ -1,18 +1,22 @@
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -PredictionSource History
 
-$promptChar = [char]0xe0b0
-$options = @{
-    PromptText = `
-        "`e[38;2;88;88;88m${promptChar}`e[0m ", `
-        "`e[91m${promptChar}`e[0m " `
-        ;
+$psReadLineVersion = [int]"$((Get-Module PSReadLine).Version.ToString())".Replace(".", "")
+if ($psReadLineVersion -gt 210) {
+    $promptChar = [char]0xe0b0
+    $options = @{
+        PromptText = `
+            "`e[38;2;88;88;88m${promptChar}`e[0m ", `
+            "`e[91m${promptChar}`e[0m " `
+            ;
+    }
+    Set-PSReadLineOption @options
+    Set-PSReadLineOption -ContinuationPrompt "`e[48;2;88;88;88m `e[0m`e[38;2;88;88;88m$([char]0xE0B0)`e[0m "
+
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineKeyHandler -ViMode Insert -Chord "Ctrl+)" -Function AcceptNextSuggestionWord
 }
-Set-PSReadLineOption @options
-Set-PSReadLineOption -ContinuationPrompt "`e[48;2;88;88;88m `e[0m`e[38;2;88;88;88m$([char]0xE0B0)`e[0m "
 
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -ViMode Insert -Chord "Ctrl+)" -Function AcceptNextSuggestionWord
 
 ## As a matter of habit, remap $ and _ keys from French AZERTY layout
 ## to the new keys from the French AZERTY-NF layout
